4534ca828535f8a0d95cad8821ed80faa551903e
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2022-02-10 10:22:29 -0300

    feat poke card

--- poke-list/src/components/PokeCard/index.jsx
+++ poke-list/src/components/PokeCard/index.jsx
@@ -23,6 +23,22 @@ const PokeCard = () => {
                     </div>
 
                 </div>
+
+                <div className="cardContainer">  
+                    <span>#001</span>
+                    
+                    <div className="infoCard">
+                        <h2>Bulbassaur</h2>
+                        <p>Grass</p>
+                        <p>Poison</p>
+                    </div>
+
+                    <div className="imagesCard">
+                        <img src="poke-color-gray.png" alt="Pokeball background"/>
+                        <img src="pokeball.png" alt="Imagem do pokemon"/>
+                    </div>
+
+                </div>
             </div>    
         </div>            
     )
